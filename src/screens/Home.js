9b494857cec916b4c8e9715b3a77238cf06ad5a2
Carlos Freitas <carlos.freitas@turtleagencia.com.br>
2019-05-31 10:47:23 -0300

    Add content

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight, Image } from 'react-native';
 
 export default class Home extends Component {
 
@@ -10,17 +10,56 @@ export default class Home extends Component {
 
   constructor(props){
     super(props);
-    this.state = {};
-
-  }
-
-  
-
+    this.state = {
+      frase: '',
+      nome: 'Aléxia'
+    };
+  }  
 
   render() {
     return(
       <View style={styles.container}>
         <Text style={styles.title}> Escolha um idioma </Text>
+
+        <View style={styles.bgCountry}>
+
+          <TouchableHighlight style={styles.btnCountry} onPress={()=>{
+            let frase = 'Eu te amo';
+            this.state.frase = frase;
+            this.setState({frase:frase});
+          }}>
+           <Image source={require('../assets/images/brazil.png')} style={styles.country} />
+          </TouchableHighlight>
+
+          <TouchableHighlight style={styles.btnCountry} onPress={()=>{
+            let frase = 'I love you';
+            this.state.frase = frase;
+            this.setState({frase:frase});
+          }}>
+           <Image source={require('../assets/images/eua.png')} style={styles.country} />
+          </TouchableHighlight>
+
+          <TouchableHighlight style={styles.btnCountry} onPress={()=>{
+            let frase = 'Kocham';
+            this.state.frase = frase;
+            this.setState({frase:frase});
+          }}>
+            <Image source={require('../assets/images/poland.png')} style={styles.country} />
+          </TouchableHighlight>
+
+          <TouchableHighlight style={styles.btnCountry} onPress={()=>{
+            let frase = 'Amo';
+            this.state.frase = frase;
+            this.setState({frase:frase});
+          }}>
+            <Image source={require('../assets/images/italy.png')} style={styles.country} />
+          </TouchableHighlight>
+          
+        </View>
+
+        <View style={styles.bgCountry}> 
+          <Text style={styles.txtFrase}>{this.state.frase+' '+this.state.nome+'!'}</Text>
+        </View>
       </View>
     );
   }
@@ -30,11 +69,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#ff0000'
+    backgroundColor: '#ffcadc'
   },
   title: {
     color: '#ffffff',
     fontSize: 20,
     marginTop: 20,
+  },
+  bgCountry: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 50,
+  },
+  btnCountry: {
+    width: 50,
+    height: 50,
+    margin: 10,
+  },
+  country: {
+    width: 50,
+    height: 50,
+  },
+  txtFrase: {
+    fontSize: 25,
+    color: '#ffffff',
   }
 });
